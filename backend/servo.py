d22486abe8d77b12905f16b233a420830382feea
hiranyagarbh <hiranyagarbh.choudhary@gmail.com>
2019-03-31 09:32:21 +0530

    servo with firebase

--- backend/servo.py
+++ backend/servo.py
@@ -4,24 +4,12 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
-#config = {
-#  "apiKey": "AIzaSyD3Zbqnk8e4xXsubLfobhBhQJ2dl3pLy-k",
-#  "authDomain": "smart-dustbin-e369d.firebaseapp.com",
-#  "databaseURL": "https://smart-dustbin-e369d.firebaseio.com/",
-#  "storageBucket":""
-#}
-
-#firebase = pyrebase.initialize_app(config)
-#db = firebase.database()
-
-#db.child("All Bins").child("Bin 1").update({"Bio_percent": 25})
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)
 
 p = GPIO.PWM(7,50)
 p.start(7.5) #NEUTRAL START - DONT CHANGE
-#db.child("All Bins").child("Bin 1").update({"Bio_percent": 25})
 a = 1
 try:
 	if a==0:
